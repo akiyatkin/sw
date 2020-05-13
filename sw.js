056fa6f83dcf397da07ec67d15196be1ee66de2f
Anton Kiyatkin <a.kiyatkin@itlf.ru>
2020-05-13 09:45:32 +0400

    fix

--- sw.js
+++ sw.js
@@ -11,17 +11,17 @@ this.addEventListener('fetch', event => {
 	if (event.request.method !== 'GET') return
 	let url = new URL(event.request.url)
 	if (url.origin !== location.origin) return
-	if (/^\/-/.test(url.pathname)) return
+	//if (/^\/-/.test(url.pathname)) return
 	if (/[&\?]t=\d+/.test(url.search)) return
 
-	let r = url.pathname.match(/\.(\w+$)/)
+	//let r = url.pathname.match(/\.(\w+$)/)
 	/*
 		Пользователь меняет: корзину, личный кабинет - no-store)
 		Администратор меняет: json, php - not-modified)
 		Программист меняет: index.tpl, js, css и / - public с версией в адресе
 		Никто не меняет: jpg, png, gif ... - public без версии в адресе
 	*/
-	if (r && !~['tpl','js','css'].indexOf(r[1].toLowerCase()) ) return
+	//if (r && !~['tpl','js','css'].indexOf(r[1].toLowerCase()) ) return
    	
 	const {
 		cache, credentials, headers, integrity, 
